a22434fd9370805b8855aa08155570d66ef5629c
Marwin Intal <marwinintal@gmail.com>
2024-10-11 09:34:16 +0800

    replace package name

--- src/Exceptions/ConfigurationException.php
+++ src/Exceptions/ConfigurationException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class ConfigurationException extends Exception
 {
-    public function __construct($message = "Unable to initialise marwinupstreamtech/hubspot-object-collections", $code = 0, Throwable $previous = null)
+    public function __construct($message = "Unable to initialise marwinintal/hubspot-object-collections", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
